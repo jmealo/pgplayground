5f2acac706dca46355c2cee1e58e09fe585066bd
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-07-20 18:06:16 -0400

    Do not try to modify system tables

--- scripts/create-extensions.sql
+++ scripts/create-extensions.sql
@@ -1,4 +1,5 @@
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements SCHEMA pg_catalog;
+-- CREATE EXTENSION IF NOT EXISTS pg_stat_statements SCHEMA pg_catalog;
+-- TODO: This tries to write to a system table and fails; let's review the best way to turn pg_stat_statements on/off
 
 CREATE EXTENSION IF NOT EXISTS plv8 SCHEMA pg_catalog;
 CREATE EXTENSION IF NOT EXISTS plls SCHEMA pg_catalog;
