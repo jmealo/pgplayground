0ce5ee4d98b26c74b35aab6e6d2671640e0bba84
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-08-04 16:10:37 -0400

    stub out spark points tables

--- sql/04-sparkpoints.sql
+++ sql/04-sparkpoints.sql
@@ -1,15 +1,35 @@
 CREATE TABLE IF NOT EXISTS sparkpoints
 (
   id serial NOT NULL,
-  student_title character varying,
-  teacher_title character varying,
-  student_description character varying,
-  teacher_description character varying,
+  student_title text,
+  teacher_title text,
+  student_description text,
+  teacher_description text,
+  subject standards_subject,
   content_area_id integer,
-  grade_level standard_grade[],
-  metadata jsonb
+  grade_level standards_grade[],
+  metadata jsonb,
+
+  PRIMARY KEY ("id")
 );
 
-ALTER TABLE sparkpoint
-OWNER TO spark;
+CREATE INDEX sparkpoints_content_area_id_idx ON sparkpoints (content_area_id);
+CREATE INDEX sparkpoints_subject_idx ON sparkpoints (subject);
+CREATE INDEX sparkpoints_metadata_gin_idx  ON sparkpoints USING GIN (metadata);
 
+CREATE TABLE IF NOT EXISTS sparkpoints_edges
+(
+  id serial NOT NULL,
+  target_asn_id char(8),
+  source_asn_id char(8),
+  rel_type text,
+  metadata jsonb,
+
+  PRIMARY KEY ("id"),
+  CONSTRAINT sparkpoints_edges_cycle_constraint UNIQUE (target_asn_id, source_asn_id, rel_type)
+);
+
+CREATE INDEX sparkpoints_edges_metadata_gin_idx  ON sparkpoints_edges USING GIN (metadata);
+CREATE INDEX sparkpoints_edges_target_asn_id_idx ON "sparkpoints_edges" (target_asn_id);
+CREATE INDEX sparkpoints_edges_source_asn_id_idx ON "sparkpoints_edges" (source_asn_id);
+CREATE INDEX sparkpoints_edges_rel_type_idx      ON "sparkpoints_edges" (rel_type);
